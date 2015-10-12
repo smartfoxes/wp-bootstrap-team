01c16589de239166247fba3de43847d925eeaf16
Andrei Filonov <andrei@smartfoxes.ca>
2015-10-11 22:30:45 -0600

    adin script for media

---
+++ admin.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+/**
+ * Loads the image management javascript
+ */
+function wp_bootstrap_team_enqueue() {
+    global $typenow;
+    if( $typenow == 'team_member' ) {
+        wp_enqueue_media();
+ 
+        // Registers and enqueues the required javascript.
+        wp_register_script( 'meta-box-image', plugin_dir_url( __FILE__ ) . 'js/admin.js', array( 'jquery' ) );
+        wp_localize_script( 'meta-box-image', 'meta_image',
+            array(
+                'title' => __( 'Choose or Upload an Image', 'wp_bootstrap_team' ),
+                'button' => __( 'Use this image', 'wp_bootstrap_team' ),
+            )
+        );
+        wp_enqueue_script( 'meta-box-image' );
+    }
+}
+
+add_action( 'admin_enqueue_scripts', 'wp_bootstrap_team_enqueue' );
---
+++ js/admin.js
@@ -0,0 +1,43 @@
+﻿/*
+ * Attaches the image uploader to the input field
+ */
+jQuery(document).ready(function($){
+ 
+    // Instantiates the variable that holds the media library frame.
+    var meta_image_frame;
+ 
+    // Runs when the image button is clicked.
+    $('.wp_bootstrap_team_button').click(function(e){
+        var targetFieldId = $(this).attr('id').replace(/\-button$/,'');
+        //console.log($(this).attr('id'));
+        //console.log(targetFieldId);
+        // Prevents the default action from occuring.
+        e.preventDefault();
+ 
+        // If the frame already exists, re-open it.
+        if ( meta_image_frame ) {
+            meta_image_frame.open();
+            return;
+        }
+ 
+        // Sets up the media library frame
+        meta_image_frame = wp.media.frames.meta_image_frame = wp.media({
+            title: meta_image.title,
+            button: { text:  meta_image.button },
+            library: { type: 'image' }
+        });
+ 
+        // Runs when an image is selected.
+        meta_image_frame.on('select', function(){
+ 
+            // Grabs the attachment selection and creates a JSON representation of the model.
+            var media_attachment = meta_image_frame.state().get('selection').first().toJSON();
+ 
+            // Sends the attachment URL to our custom image input field.
+            $('#'+targetFieldId).val(media_attachment.url);
+        });
+ 
+        // Opens the media library frame.
+        meta_image_frame.open();
+    });
+});
